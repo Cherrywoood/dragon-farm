fbcf211a5906b1a4f49facb7a1325e4e02222513
Cherrywood6918 <polina.gj1@yandex.ru>
2022-01-24 15:35:45 +0300

    last ver

--- src/compomemts/dragons/DragonCardList.jsx
+++ src/compomemts/dragons/DragonCardList.jsx
@@ -1,30 +1,31 @@
 import React, {useEffect, useState} from 'react';
-import DragonCard from "./DragonCard";
 import "../../style/DragonCardList.css"
-import {useHistory, useNavigate} from "react-router-dom";
 import axios from "axios";
+import DragonCard from "./DragonCard";
 
 
 const DragonCardList = () => {
-    const [dragons,setDragons] = useState([1,2,3,4,5,6]);
-    useEffect(()=> {
-    axios.create({
-            baseURL:'',
-            headers:{
-                'Authorization':`${localStorage.getItem('token')}`
+    const [dragons, setDragons] = useState([]);
+    useEffect(() => {
+        axios.create({
+            baseURL: '',
+            headers: {
+                'Authorization': `${localStorage.getItem('token')}`
             }
-        }).get("http://localhost:8080/dragon/130")
+        }).get("http://localhost:8080/dragon/all")
             .then(res => {
                 console.log(res)
-            }).catch((err)=>{
-                console.log(err.response)
-            })
-    })
+                setDragons(res.data)
+            }).catch((err) => {
+            console.log(err.response)
+        })
+    }, [])
     return (
+
         <div className="dragon-card-list">
-           {/* dragons.map((dragon, index) =>
-                <DragonCard name= {dragon} id={index} key={index}/>
-            )*/}
+            {dragons.map((dragon, index) =>
+                <DragonCard dragon={dragon} key={dragon.id}/>
+            )}
 
         </div>
     );
@@ -32,50 +33,3 @@ const DragonCardList = () => {
 
 export default DragonCardList;
 
-/*axios.create({
-           baseURL:'',
-           headers:{
-               'Authorization':`${localStorage.getItem('token')}`
-           }
-       }).get("http://localhost:8080/dragon/all")
-           .then(res => {
-               console.log(res)
-           }).catch((err)=>{
-           console.log(err.response)
-       })*/
-/*console.log(localStorage.getItem('token'))
-let headers = new Headers();
-
-
-headers.append('Content-Type', 'application/json');
-headers.append('Accept', 'application/json');
-
-headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
-headers.append('Access-Control-Allow-Credentials', 'true');
-
-headers.append('Authorization', `eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjE4ODA4NzUiLCJ1c2VySWQiOjF9.-hrFrPDL06_AsOv3j2bqBSracK30GbZg_4lbfu63T7U`);
-console.log(headers)
-fetch("http://localhost:8080/dragon/all", {
-   mode: 'no-cors',
-   method: 'GET',
-   headers: headers
-}).then(json => console.log(json))
-   .catch(error => console.log(error));
-
-
-axios.create({
-   baseURL:'',
-   headers:{
-       'Content-Type':'application/json',
-       'Access-Control-Allow-Origin':'http://localhost:3000',
-       'Authorization':`${localStorage.getItem('token')}`,
-       'Accept': "application/json",
-       'Access-Control-Allow-Credentials': 'true'
-   }
-}).get("http://localhost:8080/dragon/all")
-   .then(res => {
-       console.log(res)
-   }).catch((err)=>{
-       console.log(err.response)
-})
-*/
